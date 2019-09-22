5c8cdc5379bdb3c68c104cc5ebd2cf61ff7201bf
Grégory Le Garec <gregory.legarec@gmail.com>
2019-09-23 00:59:05 +0200

    feat: Add UI components based on Material-UI

--- 2-webapp/app/src/App.js
+++ 2-webapp/app/src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Container, CircularProgress } from "@material-ui/core";
+import AlertsTable from "./components/AlertsTable";
 
 export class App extends React.Component {
   componentDidMount() {
     this.props.fetchAlerts();
   }
+
   render() {
-    return <div></div>;
+    const { alerts, isLoading } = this.props;
+    return (
+      <Container p="16" width="100%">
+        <h1>Alert Manager</h1>
+        {isLoading ? (
+          <CircularProgress mx="auto" />
+        ) : (
+          <AlertsTable alerts={alerts} />
+        )}
+      </Container>
+    );
   }
 }
 
--- 2-webapp/app/src/index.js
+++ 2-webapp/app/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import App from "./App";
 import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
+    <CssBaseline />
     <App />
   </Provider>,
   document.getElementById("root")
